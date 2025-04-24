dd0b76c5eedc44702e1ee848f2b39877a0d37321
Mykiora <anthony00029@gmail.com>
2025-04-24 08:05:31 +0200

    added get_post() function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 
 def authenticate(username, password, client_id, secret_key):
-    """Sends a request to reddit to get the access token needed to authenticate.
+    """Sends a request to reddit to get the access token needed to authenticate. Token valid for 24 hours.
        Returns a dictionary containing the token and the headers.
 
     Args:
@@ -32,6 +32,28 @@ def authenticate(username, password, client_id, secret_key):
             "Authorization": f"bearer {request.json()['access_token']}"}
 
 
+def get_post(url, headers):
+    """Fetches pieces of information about a post.
+
+    Args:
+        url (str): The URL of the post to fetch.
+        headers (dict): Headers containing the user agent and the authorization token. (Look authenticate function) 
+
+    Returns:
+        dict: A dictionary containing the title, the author's post, the subreddit, the number of ups, the upvote ratio and if gilded or not.
+    """
+    post = requests.get(url, headers=headers).json()
+
+    title = post[0]["data"]["children"][0]["data"]["title"]
+    subreddit = post[0]["data"]["children"][0]["data"]["subreddit"]
+    post_text = post[0]["data"]["children"][0]["data"]["selftext"]
+    gilded = post[0]["data"]["children"][0]["data"]["gilded"]
+    ups = post[0]["data"]["children"][0]["data"]["ups"]
+    upvote_ratio = post[0]["data"]["children"][0]["data"]["upvote_ratio"]
+
+    return {"title": title, "subreddit": subreddit, "text": post_text, "gilded": gilded, "ups": ups, "upvote_ratio": upvote_ratio}
+
+
 path = ".env"
 
 if os.path.exists(path):
@@ -43,7 +65,7 @@ if os.path.exists(path):
             "password": os.environ["REDDIT_PASSWORD"]}
     
     headers = authenticate(username=user["username"], password=user["password"], client_id=user["client_id"], secret_key=user["secret_key"])
-    print(requests.get("https://oauth.reddit.com/hot", headers=headers).json())
+    print(get_post(url="https://oauth.reddit.com/r/redditdev/comments/3qbll8/429_too_many_requests.json", headers=headers))
 else:
     client_id = input("Enter your client ID (personal use script): ")
     secret_key = input("Enter your secret key: ")
